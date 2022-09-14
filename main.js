dcbf09b5ff32b4c0917ecef7913fb29511711f9e
ryan <ryan.patel21@imperial.ac.uk>
2022-09-14 23:24:45 +0800

    Made the interests section, and a basic carousell going

--- main.js
+++ main.js
@@ -8,4 +8,20 @@ for (let i = 0; i < divs.length; i++) {
 let counter = 0;
 divs[counter].style.display = "flex";
 
+btn.addEventListener('click', () => {
+    if (counter < divs.length - 1) {
+        counter++;
+    } else {
+        counter = 0;
+    }
+
+    for (let i = 0; i < divs.length; i++) {
+        if (counter === i) {
+            divs[i].style.display = "flex";
+        } else {
+            divs[i].style.display = "none";
+        }
+    }
+})
+
 
