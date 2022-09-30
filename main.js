4060df91c50a1a980f53887db91e0c0684e6553e
ryan <ryan.patel21@imperial.ac.uk>
2022-09-30 17:26:51 +0100

    Added rotating button

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 // About carousell btn
-btn = document.getElementById("next-btn-icon");
+
+const aboutList = [...document.querySelectorAll(".about-ctn")];
+
+const btn = document.querySelector(".next-btn");
+const btnIcon = document.getElementById("next-btn-icon");
+
+aboutList[0].style.display = "flex";
+
+let cnt = 0;
 btn.addEventListener("click", function() {
-    
+    aboutList[cnt].style.display = "none";
+    let rotate;
+    if (cnt == 0) {
+        cnt++;
+        rotate = "-180deg";
+    } else {
+        cnt = 0;
+        rotate = "0deg";
+    }
+    aboutList[cnt].style.display = "flex";
+    btnIcon.style.transform = `rotate(${rotate})`;
 })
